a89fc3e1cf57b695f68ea1d740f9a98fcc2cf672
Nathan West <Lucretiel@gmail.com>
2014-11-23 01:24:17 -0500

    Updates to code_jam.py
    INSERT_NEWLINE is now a global member. Set it in your code.
    Tokens no longer has __iter__. No need to iterate over all tokens.

--- python/code_jam.py
+++ python/code_jam.py
@@ -20,12 +20,15 @@ Utility library for solving code jams. Handles input tokenization and output
 formatting.
 '''
 
-from __future__ import print_function
 from sys import stdin, stdout
 from signal import signal, SIGPIPE, SIG_DFL
 from argparse import ArgumentParser
 from contextlib import contextmanager
 from inspect import isgeneratorfunction
+
+# Set this variable to true in your code to force printing newlines between
+# "Case #" and the solution itself
+INSERT_NEWLINE = False
 
 class Tokens:
     '''
@@ -43,9 +46,6 @@ class Tokens:
 
     def __init__(self, stream):
         self.tokens = self.tokenize(stream)
-
-    def __iter__(self):
-        return self.tokens
 
     def next_token(self, t):
         '''
@@ -67,7 +67,7 @@ class Tokens:
         return self.next_many_tokens(self.next_token(int), t)
 
 
-def print_code_jam(solutions, ostr, insert_newline=False):
+def print_cases(solutions, ostr):
     '''
     Format and print the solutions of a code jam to the file object `ostr`.
     `solutions` should be an ordered iterable of solutions. Prints using the
@@ -81,8 +81,8 @@ def print_code_jam(solutions, ostr, insert_newline=False):
     Output is unconditionally flushed on every solution. This is to enable
     real-time output even when in a pipeline, such as with head or tee.
     '''
+    sep = '\n' if INSERT_NEWLINE else ' '
     format_case = "Case #{}:".format
-    sep = '\n' if insert_newline else ' '
 
     try:
         for case, solution in enumerate(solutions, 1):
@@ -91,24 +91,23 @@ def print_code_jam(solutions, ostr, insert_newline=False):
         signal(SIGPIPE, SIG_DFL)
 
 
-def generator_solve_code_jam(solver, istr, ostr, insert_newline=False):
+def generator_solve_code_jam(solver, istr, ostr):
     '''
     Print the solution of a code jam to the file object `ostr`, given an input
     file object `istr`. `solver` is a generator that takes a Tokens object and
     yields solutions to each case. Handles formatting the output correctly,
-    using the standard code jam "Case #1: X" formatting. If `insert_newline` is
-    True, a newline is printed before the solution ("Case #1:\nX"). The
-    solution is outputted via a normal print, so returning strings, ints, or
-    other printable types is fine.
+    using the standard code jam "Case #1: X" formatting. The solution is
+    outputted via a normal print, so returning strings, ints, or other
+    printable types is fine.
 
     This function also silently returns in the event of a BrokenPipeError from
     either the input or output file.
     '''
 
-    return print_code_jam(solver(Tokens(istr)), ostr, insert_newline)
+    return print_cases(solver(Tokens(istr)), ostr)
 
 
-def solve_code_jam(solver, istr, ostr, insert_newline=False):
+def solve_code_jam(solver, istr, ostr):
     '''
     For a code jam where the first token is the number of cases, this function
     prints the solution to the file object `ostr`, given an input file object
@@ -120,7 +119,7 @@ def solve_code_jam(solver, istr, ostr, insert_newline=False):
         for _ in range(tokens.next_token(int)):
             yield solver(tokens)
 
-    return generator_solve_code_jam(solve, istr, ostr, insert_newline)
+    return generator_solve_code_jam(solve, istr, ostr)
 
 @contextmanager
 def _smart_open(filename, *args, **kwargs):
@@ -135,55 +134,41 @@ def _smart_open(filename, *args, **kwargs):
     else:
         yield filename
 
-def autosolve(func=None, *, insert_newline=False):
+def autosolve(solver):
     '''
     Decorator to immediatly solve a code jam with a function. It should
     decorate a function which, when called with a Tokens object, returns a
     solution to a single test case. The code jam is then immediatly solved
     by assuming the first token is the number of test cases, and repeatedly
     calling the decorated function to retreive solutions. Doesn't respect
-    __name__ == '__main__'. Can be used with or without arguments:
-
-        @autosolve
-        def solver(tokens):
-            code code code
-
-        @autosolve(insert_newline=True, ...)
-        def solver(tokens):
-            code code code
-
-    If `insert_newline` is True, a newline is printed between each "Case #" and
-    the solution itself.
+    __name__ == '__main__'.
 
     If the decorated function is a generator, the behavior is slightly diffent.
     The generator is called with the tokens object, and each yielded solution is
     printed. The generator is responsible for yielding the correct number of
-    solutions in this case
+    solutions.
 
-    autosolve also collects filenames from sys.argv. The first CLI argument, if
-    given, is the input file, and the second, if given, is the output file.
-    These default to stdin and stdout, respectively.
+    autosolve also collects filenames from sys.argv. The first command line
+    argument, if given, is the input file, and the second, if given, is the
+    output file. These default to stdin and stdout, respectively.
 
     The decorated function is returned unchanged.
     '''
-    def decorator(solver):
-        parser = ArgumentParser()
-        parser.add_argument('in_file', nargs='?', default=stdin,
-            help="The input file to use. Defaults to stdin")
-        parser.add_argument('out_file', nargs='?', default=stdout,
-            help="The file to write the solutions to. Defaults to stdout.")
-        args = parser.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument('in_file', nargs='?', default=stdin,
+        help="The input file to use. Defaults to stdin")
+    parser.add_argument('out_file', nargs='?', default=stdout,
+        help="The file to write the solutions to. Defaults to stdout.")
+    args = parser.parse_args()
 
-        with _smart_open(args.in_file, 'r', encoding='ascii') as istr:
-            with _smart_open(args.out_file, 'w', encoding='ascii') as ostr:
-                if isgeneratorfunction(solver):
-                    generator_solve_code_jam(solver, istr, ostr, insert_newline)
-                else:
-                    solve_code_jam(solver, istr, ostr, insert_newline)
+    with _smart_open(args.in_file, 'r', encoding='ascii') as istr:
+        with _smart_open(args.out_file, 'w', encoding='ascii') as ostr:
+            if isgeneratorfunction(solver):
+                generator_solve_code_jam(solver, istr, ostr)
+            else:
+                solve_code_jam(solver, istr, ostr)
 
-        return solver
-
-    return decorator(func) if func else decorator
+    return solver
 
 # TODO: Windows (sometimes) defaults to UTF-16 or some other ascii-incompatible
 # format on stdout. Force autosolve to make output to be ascii when >redirecting
