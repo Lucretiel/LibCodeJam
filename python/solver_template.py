b7aff977591d489f66649f96da58f7275d5b9c2a
Lucretiel <Lucretiel@users.noreply.github.com>
2015-03-12 17:52:56 -0400

    Comment updates to the python template

--- python/solver_template.py
+++ python/solver_template.py
@@ -32,13 +32,11 @@ def solve_case(tokens):
     Otherwise, it should return a single solution; @autosolve will call it the
     correct number of times.
 
-    A token is simply a whitespace-delimited string, such as via str.split. The
-    tokens object makes no distinction between tokens on the same line or
-    different lines.
-
     Running this file will solve a code jam problem, taking the input file from
-    stdin and writing the solution to stdout. The tokens object has the
-    following methods:
+    stdin and writing the solution to stdout. The input is split into tokens
+    using the Tokens class; a token is simply a whitespace-delimited string,
+    such as via str.split. The tokens object makes no distinction between tokens
+    on the same line or different lines. It has the following methods:
 
     tokens.next_token(type) -> get the next token, converting it to type
     tokens.next_many_tokens(n, type) -> yield the next n tokens
@@ -50,6 +48,6 @@ def solve_case(tokens):
     Optionally, add additional parameters to the solve_case function. One token
     will be extracted for each parameter besides tokens, in order, and passed
     in as additional arguments. Use a type annotation to specify the tokens's
-    type; this will be passed into. Remove the tokens parameter if you won't
-    need to extract any extra tokens.
+    type; this will be passed into tokens.next_token (defaulting to str). Remove
+    the tokens parameter if you won't need to extract any extra tokens.
     '''
