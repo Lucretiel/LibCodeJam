a0456cc831666663ee492b63e422abb405412a65
Lucretiel <Lucretiel@users.noreply.github.com>
2015-03-12 17:46:27 -0400

    Added @collects_tokens to the python template.

--- python/solver_template.py
+++ python/solver_template.py
@@ -2,9 +2,13 @@
 # https://github.com/Lucretiel/LibCodeJam for source code and (ostensibly) some
 # documentation.
 
-from code_jam import autosolve
+from code_jam import autosolve, collects_tokens
 
-# Uncomment if you want a newline between the "Case #" and the actual solution:
+
+# Uncomment if you want a newline between the "Case #" and the actual solution
+#code_jam.INSERT_NEWLINE = True
+
+# Example:
 #
 #    Case #1: solution
 #    Case #2: solution
@@ -17,9 +21,8 @@ from code_jam import autosolve
 #    solution
 #
 
-#code_jam.INSERT_NEWLINE = True
-
 @autosolve
+@collects_tokens
 def solve_case(tokens):
     '''
     Solve a single test case using the tokens, and return the solution. If
@@ -28,14 +31,25 @@ def solve_case(tokens):
     Language), this function should be a generator which yields each solution.
     Otherwise, it should return a single solution; @autosolve will call it the
     correct number of times.
-    
-    This will solve a code jam, taking the input file from stdin and writing the
-    solution to stdout. The tokens object has the follwing methods:
-    
+
+    A token is simply a whitespace-delimited string, such as via str.split. The
+    tokens object makes no distinction between tokens on the same line or
+    different lines.
+
+    Running this file will solve a code jam problem, taking the input file from
+    stdin and writing the solution to stdout. The tokens object has the
+    following methods:
+
     tokens.next_token(type) -> get the next token, converting it to type
     tokens.next_many_tokens(n, type) -> yield the next n tokens
     tokens.next_counted_tokens(type) -> get the next int token, then yield that
         many tokens
-    
-    A token is simply a whitespace-delimited string, such as via str.split.
+
+    These methods have the aliases n, m, and c, respectively.
+
+    Optionally, add additional parameters to the solve_case function. One token
+    will be extracted for each parameter besides tokens, in order, and passed
+    in as additional arguments. Use a type annotation to specify the tokens's
+    type; this will be passed into. Remove the tokens parameter if you won't
+    need to extract any extra tokens.
     '''
