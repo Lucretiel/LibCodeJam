395a3a3e3dea574dd57da2c575e38c9f351928d2
Nathan West <Lucretiel@gmail.com>
2016-03-13 18:27:24 -0400

    Added minimal python template

---
+++ python/minimal_template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# This code jam solution is powered by Nathan West's LibCodeJam; see
+# https://github.com/Lucretiel/LibCodeJam for source code and (ostensibly) some
+# documentation.
+
+from code_jam import *
+
+#code_jam.INSERT_NEWLINE = True
+
+# quick reference:
+#   @autosolve, @collects, @cases(n)
+#   tokens.token(t), tokens.many(n, t)
+#   debug(expr), @unroll(t)gen
+
+@autosolve
+@collects
+def solve(tokens):
+    pass
